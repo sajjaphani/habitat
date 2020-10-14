85e4a9e2ee491df46f9d79aa0918a2f0202e0aac
Christopher Maier <cmaier@chef.io>
2020-10-14 13:26:33 -0400

    Modification of powershell and environment variable usage
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .expeditor/scripts/end_to_end/run_e2e_test.ps1
+++ .expeditor/scripts/end_to_end/run_e2e_test.ps1
@@ -1,8 +1,14 @@
 param (
-    [string]$Channel = "dev",
+    [Parameter(Mandatory=$true, Position=0)]
     [string]$TestName,
+    [string]$Channel = "$env:HAB_BLDR_CHANNEL",
     [string]$BuilderUrl = $env:HAB_BLDR_URL
 )
+
+
+Write-Host "Channel = $Channel"
+Write-Host "TestName = $TestName"
+Write-Host "BuilderUrl = $BuilderUrl"
 
 . .expeditor/scripts/shared.ps1
 . .expeditor/scripts/end_to_end/setup_environment.ps1 $Channel $BuilderUrl
--- .expeditor/scripts/end_to_end/run_e2e_test.sh
+++ .expeditor/scripts/end_to_end/run_e2e_test.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 channel=${1:?You must specify a channel value}
 test_name=${2:-}
 
+echo "Using channel: $channel"
+echo "Using test_name: $test_name"
+
 source .expeditor/scripts/end_to_end/setup_environment.sh "$channel"
 if [ -n "$test_name" ]; then
     pwsh .expeditor/scripts/end_to_end/run_e2e_test_core.ps1 "$test_name"
